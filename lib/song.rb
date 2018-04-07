78a02fcbc7ba7722cadc31928a4c3fe73ea25d5b
briecodes <hello@brie.work>
2018-04-07 17:40:24 -0400

    Song class defined.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +11,51 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    self.new_by_name(name)
+  end
+
+  def self.find_by_name(name)
+    self.all.find{|song|song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by{|song|song.name}
+  end
+
+  def self.new_from_filename(file_name)
+    song = self.find_or_create_by_name(file_name.split(" - ")[1].split(".")[0])
+    song.artist_name = file_name.split(" - ")[0]
+    song
+  end
+
+  def self.create_from_filename(file_name)
+    song = self.find_or_create_by_name(file_name.split(" - ")[1].split(".")[0])
+    song.artist_name = file_name.split(" - ")[0]
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
